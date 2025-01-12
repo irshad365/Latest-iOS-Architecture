0f09bab47f1accbe3a77e3e13dddadd534a56b11
Mohamed Irshad <irshad365@gmail.com>
2025-01-12 18:12:15 -0500

    Initial Commit

---
+++ UserManagement/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UserManagement
+//
+//  Created by Mohamed Irshad on 1/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UserManagement/UserManagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserManagementApp.swift
+//  UserManagement
+//
+//  Created by Mohamed Irshad on 1/12/25.
+//
+
+import SwiftUI
+
+@main
+struct UserManagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
